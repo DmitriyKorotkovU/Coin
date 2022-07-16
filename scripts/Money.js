a3b66e71e51157890768445d378bb94236d554eb
DmitriyKorotkovU <dimka-13-01@mail.ru>
2022-07-16 15:10:45 +0300

    A lot of alterations

--- scripts/Money.js
+++ scripts/Money.js
@@ -1,18 +1,29 @@
 let randMon=[
-"ОРЁЛ", 
-"РЕШКА"
+{engl: "HEADS", rusl: "(орёл)"},
+{engl: "TAILS", rusl: "(решка)"}
 ];
 
 function randfun(arr){
     let randName = Math.floor(Math.random() * arr.length);
-						return arr[randName];
+		return arr[randName];               
 } 
+
+
 let monetka = document.querySelector(".money");
+let rusw = document.querySelector(".rusw");
 let button = document.querySelector(".button");
+
+
+
 button.addEventListener("click", function() {
-  monetka.textContent=randfun(randMon);
+  
+  let randmonet = randfun(randMon);
+  monetka.textContent=randmonet.engl;
+  rusw.textContent=randmonet.rusl;
+  
 });
 
+  
 let score=document.querySelector(".score");
 let buton=document.querySelector(".buton");
 let startscore=0;
@@ -33,7 +44,8 @@ let buttontwo = document.querySelector(".buttontwo");
 let restart = document.querySelector(".restart");
 
 buttontwo.addEventListener("click", function(){
-      monetka.textContent="ОРЁЛ ИЛИ РЕШКА?";
+      monetka.textContent="HEADS or TAILS?";
+      rusw.textContent="(орёл или решка?)"
       startscore=0;
       score.textContent=startscore;
       startscoree=0;
